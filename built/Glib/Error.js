0e3fdced12244d4a90119f5d3771f78768066948
gavin <gavinrvsmith@gmail.com>
2024-01-06 12:43:28 -0800

    del js

--- built/Glib/Error.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CustomError = void 0;
-class CustomError extends Error {
-    constructor(...message) { super(message.map(m => String(m)).join(' ')); this.name = this.constructor.name; }
-}
-exports.CustomError = CustomError;
--- built/Glib/Sort.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Sorts = void 0;
-class Sorts {
-    static LeastFirst = (a, b) => a - b;
-    static GreatestFirst = (a, b) => b - a;
-    static Alphabetical = (a, b) => a.localeCompare(b);
-}
-exports.Sorts = Sorts;
