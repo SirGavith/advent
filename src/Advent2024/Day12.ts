4552c8a70007196da1a22ef750c91305262f3b86
gavin <gavinrvsmith@gmail.com>
2024-12-22 21:25:20 -0800

    day12 speed

--- src/Advent2024/Day12.ts
+++ src/Advent2024/Day12.ts
@@ -2,7 +2,7 @@ import { Array2D, XY } from "../Glib/XY";
 import { DataFull } from "../main";
 
 
-const arr = Array2D.fromString(DataFull).Log()
+const arr = Array2D.fromString(DataFull)
 
 
 const unfilledgrid = new Set(arr.Size.minus(1).Combinations().map(c => c.toString()))
@@ -18,6 +18,8 @@ while (unfilledgrid.size > 0) {
     const unvisited = new Set<string>([xystr])
     const plants = new Set<string>()
     let edges = 0
+
+    const minXY = new XY, maxXY = new XY
     
     while (unvisited.size > 0) {
         const [nxystr] = unvisited
@@ -25,7 +27,12 @@ while (unfilledgrid.size > 0) {
         unfilledgrid.delete(nxystr)
         plants.add(nxystr)
         const nxy = XY.fromString(nxystr)
-        
+
+        if (nxy.X < minXY.X) minXY.X = nxy.X
+        if (nxy.Y < minXY.Y) minXY.Y = nxy.Y
+        if (nxy.X > maxXY.X) maxXY.X = nxy.X
+        if (nxy.Y > maxXY.Y) maxXY.Y = nxy.Y
+
         let nedges = 0
         
         arr.Neighbours(nxy).forEach(([nnxy, nnname]) => {
@@ -42,25 +49,23 @@ while (unfilledgrid.size > 0) {
 
     let sides = 0
 
-    for (let x = 0; x <= arr.Size.X; x++) {
+    for (let x = minXY.X; x <= maxXY.X + 1; x++) {
         //find num of sides at x=x
         let onEdge = false
         let isLeft = false
 
-        for (let y = 0; y < arr.Size.Y; y++) {
+        for (let y = minXY.Y; y < maxXY.Y + 1; y++) {
             const l = plants.has(new XY(x - 1, y).toString())
             const r = plants.has(new XY(x, y).toString())
 
-            const exactlyOneInPlants = (l && !r) || (!l && r)
-
-            if (onEdge && (!exactlyOneInPlants || (!l === isLeft))) {
+            if (onEdge && (r === l || (!l === isLeft))) {
                 //neither or both are name; so end edge.
                 //Alternatively, we switch what side the edge is on, so end edge and we will start another
                 onEdge = false
                 sides++
             }
             
-            if (exactlyOneInPlants) {
+            if (r !== l) {
                 onEdge = true
                 isLeft = l
             }
@@ -71,25 +76,23 @@ while (unfilledgrid.size > 0) {
 
     // console.log('vertsides', sides)
 
-    for (let y = 0; y <= arr.Size.Y; y++) {
+    for (let y = minXY.Y; y <= maxXY.Y + 1; y++) {
         //find num of sides at y=y
         let onEdge = false
         let isLeft = false
         
-        for (let x = 0; x < arr.Size.X; x++) {
-            const t = plants.has(new XY(x, y - 1).toString())
-            const b = plants.has(new XY(x, y).toString())
+        for (let x = minXY.X; x < maxXY.X + 1; x++) {
+            const t = plants.has(x + ',' + (y - 1))
+            const b = plants.has(x + ',' + y)
 
-            const exactlyOneInPlants = (t && !b) || (!t && b)
-
-            if (onEdge && (!exactlyOneInPlants || (!t === isLeft))) {
+            if (onEdge && (t === b || (!t === isLeft))) {
                 //neither or both are name; so end edge.
                 //Alternatively, we switch what side the edge is on, so end edge and we will start another
                 onEdge = false
                 sides++
             }
 
-            if (exactlyOneInPlants) {
+            if (t !== b) {
                 onEdge = true
                 isLeft = t
             }
@@ -97,7 +100,6 @@ while (unfilledgrid.size > 0) {
 
         if (onEdge) sides++
     }
-
 
     // console.log(name, xy, 'sides:', sides, 'price', sides * plants.size)
     price += sides * plants.size
